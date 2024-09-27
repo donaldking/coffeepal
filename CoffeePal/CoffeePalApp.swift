9e292d5e766beac3d28fba168693bf60750a5094
Donald King <donaldthedeveloper@gmail.com>
2024-09-27 12:54:37 +0100

    Initial Commit

---
+++ CoffeePal/CoffeePalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeePalApp.swift
+//  CoffeePal
+//
+//  Created by Mac User on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CoffeePalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
